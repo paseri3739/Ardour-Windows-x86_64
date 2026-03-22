740f273416c0df46bbb7f04ffc47b4dbcbd74ff6
Masato Suzuki <tael43@icloud.com>
2026-03-22 18:31:09 +0900

    flake

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "Minimal devShell for Ardour-Windows-x86_64";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = [
+          pkgs.git
+        ];
+      };
+    };
+}
